6230e50d7bafb89ac8dce8091f96f3aaaa8149ff
chrisarme <christopher.arme@gmail.com>
2018-01-30 15:29:46 -0600

    Added Hello World Project

---
+++ Hello World/Project1/Source.cpp
@@ -0,0 +1,16 @@
+// Similiar to "import" in java. This "imports" the iostream 
+#include <iostream>
+
+// This uses the namespace std (Standard Library) and allows for shortened calls
+using namespace std;
+
+// This is the starting point of the program. Everything branches out from here
+int main()
+{
+	// cout = console output
+	// This line outputs "Hello, World!" with a new line afterwads into the console output
+	cout << "Hello, World!\n";
+
+	// Sends a return code letting the user know everything is OK
+	return 0;
+}
