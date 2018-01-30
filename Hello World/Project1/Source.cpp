d3c9d8d59369a3bdab5a4389e4d67c7f5f71562c
chrisarme <christopher.arme@gmail.com>
2018-01-30 16:40:46 -0600

    Added Lab 1

--- Hello World/Project1/Source.cpp
+++ Hello World/Project1/Source.cpp
@@ -1,14 +1,15 @@
 // Similiar to "import" in java. This "imports" the iostream 
 #include <iostream>
 
-// This uses the namespace std (Standard Library) and allows for shortened calls
+// This uses the namespace std (Standard, as in the standard library) and allows for shortened calls
+// All following code will use the objects declared in "std" namespace
 using namespace std;
 
 // This is the starting point of the program. Everything branches out from here
 int main()
 {
 	// cout = console output
-	// This line outputs "Hello, World!" with a new line afterwads into the console output
+	// This line outputs a string containing "Hello, World!" with a new line afterwards into the console output
 	cout << "Hello, World!\n";
 
 	// Sends a return code letting the user know everything is OK
