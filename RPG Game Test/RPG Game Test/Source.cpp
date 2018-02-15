217dd7baa817b672fbcf3aab069b79a4ae03c2ce
chrisarme <christopher.arme@gmail.com>
2018-02-15 16:50:12 -0600

    Worked on RPG Game

--- RPG Game Test/RPG Game Test/Source.cpp
+++ RPG Game Test/RPG Game Test/Source.cpp
@@ -8,40 +8,107 @@ using std::vector;
 
 class Player
 {
+	vector<string> bag;
 
+public:
+
+	void addItemToBad(string object)
+	{
+		bag.push_back(object);
+	}
+
+	void displayInventory()
+	{
+		for (string item : bag)
+		{
+			cout << item << endl;
+		}
+	}
 };
 
 class GameController
 {
-	
+	Player player;
+	vector<string> commandList;
+public:
+
+	GameController()
+	{
+		commandList.push_back("add");
+		commandList.push_back("drop");
+		commandList.push_back("grab");
+		commandList.push_back("show");
+	}
+
+	string parseInput(string rawInput)
+	{
+		string parseString;
+
+		for (int i = 0; i < rawInput.length(); i++)
+		{
+			if (rawInput[i] != ' ')
+			{
+				parseString = parseString + rawInput[i];
+			}
+		}
+
+		for (int i = 0; i < parseString.length(); i++)
+		{
+			parseString[i] = tolower(parseString[i]);
+		}
+
+		return parseString;
+	}
+
+	void readForCommand(string playerInput)
+	{
+		string inputSubstr;
+
+		for (string command: commandList)
+		{
+			if (playerInput.length() >= command.length())
+			{
+				inputSubstr = playerInput.substr(0, command.length());
+				
+				if (inputSubstr == command)
+				{
+					if (command == "add")
+					{
+						player.addItemToBad(playerInput.substr(command.length(), playerInput.length()));
+					}
+
+					cout << "Got It!" << endl << endl;
+					if (command == "show")
+					{
+						player.displayInventory();
+					}
+				}
+			}
+		}
+	}
+
+	void checkAndRespondToInput()
+	{
+		string playerInput;
+		string parsedPlayerChoice;
+		string choiceCheck;
+
+		cin.clear();
+		getline(cin, playerInput);
+
+		parsedPlayerChoice = parseInput(playerInput);
+
+		readForCommand(parsedPlayerChoice);
+	}
 };
 
 int main()
 {
-	GameController gameController;
-	Player player;
-	string playerChoice;
-	string choiceCheck;
+	GameController gameController = GameController();
 
 	while (true)
 	{
-		cin.clear();
-		getline(cin, playerChoice);
-		
-		for (int i = 0; i < playerChoice.length(); i++)
-		{
-			playerChoice[i] = tolower(playerChoice[i]);
-		}
-
-		choiceCheck = playerChoice.substr(0, 5);
-
-		if (choiceCheck.find("add") != string::npos)
-		{
-			if (choiceCheck.substr(0, 4) == "add ")
-			{
-				cout << "Got It!" << endl;
-			}
-		}
+		gameController.checkAndRespondToInput();
 
 		system("PAUSE");
 	}
