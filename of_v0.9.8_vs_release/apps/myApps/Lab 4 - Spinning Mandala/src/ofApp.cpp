647a19ad79d9943479d51636093bc55c65c8e6c4
chrisarme <christopher.arme@gmail.com>
2018-02-23 12:13:37 -0600

    Started Lab 4

---
+++ of_v0.9.8_vs_release/apps/myApps/Lab 4 - Spinning Mandala/src/ofApp.cpp
@@ -0,0 +1,118 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+	ofSetBackgroundColor(255);
+	
+	myFont.load("Early GameBoy.ttf", 20);
+
+	// ofTrueTypeFont & drawString(signiture)
+	// PUT FONTS / IMAGES INTO C:\Users\chris\Documents\GitHub\CRCP-3305\of_v0.9.8_vs_release\apps\myApps\Lab 4 - Spinning Mandala\bin\data
+	// IN ofApp.h: ADD ofTrueTypeFont myfont;
+	// AFTER, PUT INTO SETUP IN HERE myFont.load(fontName, size);
+	// ex: myFont.drawString("Hello World!", xpos, ypos);
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+
+	shapeRotate += .1;
+	squarePosition += .1;
+
+	if (abs(squarePosition) >= 350)
+	{
+		squarePosition = 0;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+
+	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+
+	// 18 hollow square ring
+	ofSetColor(0);
+	ofSetLineWidth(3);
+	ofNoFill();
+
+	for (int i = 0; i < 18; i++)
+	{
+		ofPushMatrix();
+			ofRotate((20 * i) + shapeRotate);
+			ofDrawRectangle(100 - squarePosition, 100 - squarePosition, 150, 150);
+		ofPopMatrix();
+	}
+
+	// 6 inside circles
+	ofSetColor(0);
+	ofSetLineWidth(1);
+	ofFill();
+
+	for (int i = 0; i < 6; i++)
+	{
+		ofPushMatrix();
+			ofRotate((60 * i) - (shapeRotate * 2));
+			ofDrawCircle(50, 50, 25);
+		ofPopMatrix();
+	}
+	
+	myFont.drawString("Chris Arme", 0, 350);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
